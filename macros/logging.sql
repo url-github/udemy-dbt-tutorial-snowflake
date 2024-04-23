9608dde2a09eab062c91fcbd11e4babb987529a5
piotr.m <pmackowka@gmail.com>
2024-04-23 14:06:29 +0200

    90.Wyłączanie komunikatów dziennika

--- macros/logging.sql
+++ macros/logging.sql
@@ -1,4 +1,6 @@
 {% macro learn_logging() %}
-    {# {{ log("Call your dad!", info=True) }} #}
-
+    {{ log("Call your mom!") }}
+    {{ log("Call your dad!", info=True) }} --> Logs to the screen, too
+--  {{ log("Call your dad!", info=True) }} --> This will be put to the screen
+    {# log("Call your dad!", info=True) #} --> This won't be executed
 {% endmacro %}
